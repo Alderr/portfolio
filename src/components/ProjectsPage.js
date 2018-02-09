aab701840aa4f868f38639d319cc0343e3a5d5c8
Vernon Mensah <vernonmensah@gmail.com>
2018-02-09 04:12:07 -0500

    Boilerplate for projects

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -3,14 +3,24 @@ import React, { Component } from 'react';
 import Navigation from './Navigation';
 
 import '../css/fonts.css';
+import '../css/projects.css';
 
 
 
 class Projects extends Component {
   render() {
     return (
-      <div className=''>
-        <h1> Projects </h1>
+      <div className='projectPage-container'>
+        <div className='title-container'><h1 className='title'> Projects </h1></div>
+        <div className='projects'>
+          <div className='project-container'> </div>
+          <div className='project-container'> </div>
+          <div className='project-container'> </div>
+          <div className='project-container'> </div>
+          <div className='project-container'> </div>
+          <div className='project-container'> </div>
+          <div className='project-container'> </div>
+        </div>
         <Navigation />
       </div>
     );
