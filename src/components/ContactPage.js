3fab0380ef82c517ca1a814435f08f255f4e5129
Vernon Mensah <vernonmensah@gmail.com>
2018-02-09 03:49:23 -0500

    Add navigation to more pages! :boom:

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
+import Navigation from './Navigation';
+
 import '../css/fonts.css';
+
+
 
 class Contact extends Component {
   render() {
     return (
       <div className=''>
         <h1> Contact </h1>
+        <Navigation />
       </div>
     );
   }
--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
+import Navigation from './Navigation';
+
 import '../css/fonts.css';
+
+
 
 class Projects extends Component {
   render() {
     return (
       <div className=''>
         <h1> Projects </h1>
+        <Navigation />
       </div>
     );
   }
