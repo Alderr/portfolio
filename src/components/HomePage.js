e99d33aeee2fe2ef46ae7a6830d12f4f1bacf8ca
Vernon Mensah <vernonmensah@gmail.com>
2018-02-08 23:32:03 -0500

    Added template, some fonts, and some content for HomePage

---
+++ src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../css/fonts.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="homePage">
+        <h1 className="mainQoute">
+          <span>honestly, </span>
+          <span>I</span>
+          <span>get way too </span>
+          <span>excited</span>
+          <span>turning awesome ideas</span>
+          <span>into amazing </span>
+          <span>products</span>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
