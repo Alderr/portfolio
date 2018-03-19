26b86dd365b43a7b7824ddb9462b21eff3a3ef2a
Vernon Mensah <vernonmensah@gmail.com>
2018-03-18 22:21:23 -0400

    few adjustments to css for new projects page

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -11,19 +11,22 @@ function Projects() {
     <div className="projectPage-container">
       <div className="title-container"><h1 className="title"> [ Projects ] </h1></div>
       <div className="projects">
-        <div className="project-container c1">
+        <div className="project-container">
+          <div className="project-image c1"></div>
           <div className="proj project-one-container">
             <h1>Mailbox</h1>
             <p className="description">Amazon SES is amazing for email-marketing.</p>
           </div>
         </div>
-        <div className="project-container c2">
+        <div className="project-container">
+          <div className="project-image c2"></div>
           <div className="proj project-two-container">
             <h1 className="title-c2">_Krypto</h1>
             <p className="description">Your cryptocurrency bestfriend.</p>
           </div>
         </div>
-        <div className="project-container c3">
+        <div className="project-container">
+          <div className="project-image c3"></div>
           <div className="proj project-three-container">
             <h1 className="title-c3">fonts</h1>
             <p className="description">Fonts, fonts, fonts.</p>
