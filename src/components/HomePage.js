8f9dc0146759c770b9b8c35e3cb6a92aac1a8273
Vernon Mensah <vernonmensah@gmail.com>
2018-04-23 12:27:42 -0400

    reorder

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Projects from './ProjectsPage';
 
-import '../css/fonts.css';
 import '../css/home.css';
 
 function Home() {
@@ -11,12 +10,24 @@ function Home() {
       <div className="homePage">
         <div className="mainQouteWrapper">
           <div className="mainQoute">
-            <h1 className="firstWord">honestly, </h1>
+            {/* <h1 className="firstWord">honestly, </h1>
             <h1 className="qoute secondWord"><span className="iLetter">I</span> get way too <span className="excited">excited</span></h1>
             <h1 className="qoute">turning <span className="awesomeIdeas">ideas</span> into</h1>
             <h1 className="qoute">amazing <span className="products">products.</span> </h1>
             <h1 className="qoute flaw">It&apos;s one thing</h1>
-            <h1 className="qoute flaw-second">I wouldn&apos;t change about myself.</h1>
+            <h1 className="qoute flaw-second">I wouldn&apos;t change about myself.</h1> */}
+
+            <h1>
+              <span className="firstWord">honestly,</span>
+              <br />
+              <span className="qoute secondWord"><span className="iLetter">I</span> get way too <span className="excited"> excited</span></span>
+              <span className="qoute"> turning <span className="awesomeIdeas"> ideas</span> into</span>
+              <span className="qoute"> amazing <span className="products"> products.</span> </span>
+              <br />
+              <br />
+              <span className="qoute flaw">It&apos;s one thing</span>
+              <span className="qoute flaw-second"> I wouldn&apos;t change about myself.</span>
+            </h1>
           </div>
         </div>
       </div>
