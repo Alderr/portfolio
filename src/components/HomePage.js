68f556f754509c2a830c89c9d024ea3eb1ae5fd5
Vernon Mensah <vernonmensah@gmail.com>
2018-04-23 13:02:55 -0400

    desktop view for home and edits on mobile

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -20,13 +20,14 @@ function Home() {
             <h1>
               <span className="firstWord">honestly,</span>
               <br />
-              <span className="qoute secondWord"><span className="iLetter">I</span> get way too <span className="excited"> excited</span></span>
+              <span className="qoute secondWord"><span className="iLetter green">I</span> get way too <span className="excited"> excited</span></span>
               <span className="qoute"> turning <span className="awesomeIdeas"> ideas</span> into</span>
-              <span className="qoute"> amazing <span className="products"> products.</span> </span>
+              <span className="qoute"> amazing<span className="products"> products</span></span>.
               <br />
-              <br />
-              <span className="qoute flaw">It&apos;s one thing</span>
-              <span className="qoute flaw-second"> I wouldn&apos;t change about myself.</span>
+              <div className="reflection">
+                <span className="qoute flaw">It&apos;s one thing</span>
+                <span className="qoute flaw-second"><span className="green"> I</span> wouldn&apos;t change about<span className="green"> myself</span>.</span>
+              </div>
             </h1>
           </div>
         </div>
