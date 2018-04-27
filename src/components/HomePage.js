40511b020581631ef90383a8643468480cf88c05
Vernon Mensah <vernonmensah@gmail.com>
2018-04-27 06:40:20 -0400

    adjustments done on home-page; mobile + desktop

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,31 +4,17 @@ import '../css/home.css';
 
 function Home() {
   return (
-    <section>
-      <section className="homePage">
-        <section className="mainQouteWrapper">
-          <h1 className="qoute part-one"> vernon mensah. <span className="job-title">full stack developer</span>. </h1>
-          <h1 className="qoute part-two"> I get excited turning ideas into amazing products. </h1>
-          <h1 className="qoute part-three"> mobile. web. saas. startups. healthcare. </h1>
-        </section>
+    <section className="home-page">
+      <section className="main-qoute-wrapper">
+        <div className="qoute">
+          <div className="part-one"><h1>vernon mensah. <span className="job-title blue">full stack developer</span>. </h1></div>
+          <div className="part-two"><h1>I get excited turning ideas into </h1></div>
+          <div className="part-three"><h1>amazing products.</h1></div>
+          <div className="part-four"><h1><span className="blue">mobile</span>. <span className="blue">web</span>. <span className="blue">saas</span>. <span className="blue">startups</span>. <span className="blue">businesses</span>. </h1></div>
+        </div>
       </section>
     </section>
   );
 }
 
 export default Home;
-{ /*
-  <section className="mainQoute">
-<h1>
-  <span className="firstWord">honestly,</span>
-  <br />
-  <span className="qoute secondWord"><span className="iLetter green">I</span> get way too <span className="excited"> excited</span></span>
-  <span className="qoute"> turning <span className="awesomeIdeas"> ideas</span> into</span>
-  <span className="qoute"> amazing<span className="products"> products</span></span>.
-  <br />
-  <section className="reflection">
-    <span className="qoute flaw">It&apos;s one thing</span>
-    <span className="qoute flaw-second"><span className="green"> I</span> wouldn&apos;t change about<span className="green"> myself</span>.</span>
-  </section>
-</h1>
-</section> */ }
