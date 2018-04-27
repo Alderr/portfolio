6b167fce4baf2fbcae5e25d995a0a1bdb093336d
Vernon Mensah <vernonmensah@gmail.com>
2018-04-27 04:51:24 -0400

    markup for homePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,20 +7,9 @@ function Home() {
     <section>
       <section className="homePage">
         <section className="mainQouteWrapper">
-          <section className="mainQoute">
-            <h1>
-              <span className="firstWord">honestly,</span>
-              <br />
-              <span className="qoute secondWord"><span className="iLetter green">I</span> get way too <span className="excited"> excited</span></span>
-              <span className="qoute"> turning <span className="awesomeIdeas"> ideas</span> into</span>
-              <span className="qoute"> amazing<span className="products"> products</span></span>.
-              <br />
-              <section className="reflection">
-                <span className="qoute flaw">It&apos;s one thing</span>
-                <span className="qoute flaw-second"><span className="green"> I</span> wouldn&apos;t change about<span className="green"> myself</span>.</span>
-              </section>
-            </h1>
-          </section>
+          <h1 className="qoute part-one"> vernon mensah. <span className="job-title">full stack developer</span>. </h1>
+          <h1 className="qoute part-two"> I get excited turning ideas into amazing products. </h1>
+          <h1 className="qoute part-three"> mobile. web. saas. startups. healthcare. </h1>
         </section>
       </section>
     </section>
@@ -28,3 +17,18 @@ function Home() {
 }
 
 export default Home;
+{ /*
+  <section className="mainQoute">
+<h1>
+  <span className="firstWord">honestly,</span>
+  <br />
+  <span className="qoute secondWord"><span className="iLetter green">I</span> get way too <span className="excited"> excited</span></span>
+  <span className="qoute"> turning <span className="awesomeIdeas"> ideas</span> into</span>
+  <span className="qoute"> amazing<span className="products"> products</span></span>.
+  <br />
+  <section className="reflection">
+    <span className="qoute flaw">It&apos;s one thing</span>
+    <span className="qoute flaw-second"><span className="green"> I</span> wouldn&apos;t change about<span className="green"> myself</span>.</span>
+  </section>
+</h1>
+</section> */ }
