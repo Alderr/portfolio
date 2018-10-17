53a1a93e3a31c13d1d8f4fe2b2ff289026d6852d
Vernon Mensah <vernonmensah@gmail.com>
2018-10-17 10:24:37 -0400

    add the spaces back

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,26 +8,17 @@ function Home() {
       <section className="main-qoute-wrapper">
         <div className="qoute">
           <div className="part-one">
-            <h1>
-vernon mensah.
-              <span className="job-title blue">full stack developer</span>
-.
-            </h1>
+            <h1>vernon mensah.<span className="job-title blue">full stack developer</span>.{' '}</h1>
           </div>
           <div className="part-two"><h1>I get excited turning ideas into </h1></div>
           <div className="part-three"><h1>amazing products.</h1></div>
           <div className="part-four">
             <h1>
-              <span className="blue">mobile</span>
-.
-              <span className="blue">web</span>
-.
-              <span className="blue">saas</span>
-.
-              <span className="blue">startups</span>
-.
-              <span className="blue">businesses</span>
-.
+              <span className="blue">mobile</span>.{' '}
+              <span className="blue">web</span>.{' '}
+              <span className="blue">saas</span>.{' '}
+              <span className="blue">startups</span>.{' '}
+              <span className="blue">businesses</span>.{' '}
             </h1>
           </div>
         </div>
@@ -37,4 +28,3 @@ vernon mensah.
 }
 
 export default Home;
-
