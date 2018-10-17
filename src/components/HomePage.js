5511401765cbf4c692e6fc25b04549dd9cc0f361
Vernon Mensah <vernonmensah@gmail.com>
2018-10-17 11:56:18 -0400

    add the space!

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,7 +8,7 @@ function Home() {
       <section className="main-qoute-wrapper">
         <div className="qoute">
           <div className="part-one">
-            <h1>vernon mensah.<span className="job-title blue">front end developer</span>.{' '}</h1>
+            <h1>vernon mensah.{' '}<span className="job-title blue">front end developer</span>.{' '}</h1>
           </div>
           <div className="part-two"><h1>I get excited turning ideas into </h1></div>
           <div className="part-three"><h1>amazing products.</h1></div>
